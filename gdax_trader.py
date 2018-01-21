508f6d943ebe52808ba0cd251714ac662fda19e8
Ben Lefley <benjaminlefley@s-28-165.flex.volo.net>
2018-01-20 19:21:43 -0600

    added ticker data for gdax

--- gdax_trader.py
+++ gdax_trader.py
@@ -1,4 +1,5 @@
 import gdax
+import time
 # Python docs: https://github.com/danpaquin/gdax-python
 # GDAX docs: https://docs.gdax.com/?python#rate-limits
 
@@ -15,7 +16,30 @@ def login_authenticated(key, b64secret, passphrase):
     # currently in sandbox mode
     return gdax.AuthenticatedClient(key, b64secret, passphrase, api_url="https://api-public.sandbox.gdax.com") # these params are given once gdax account is configured with api
 
+class myWebsocketClient(gdax.WebsocketClient):
+    def on_open(self):
+        self.url = "wss://ws-feed.gdax.com/"
+        self.products = ["BTC-USD"]
+        self.message_count = 0
+        print("Lets count the messages!")
+    def on_message(self, msg):
+        self.message_count += 1
+        if 'price' in msg and 'type' in msg:
+            print ("Message type:", msg["type"],
+                   "\t@ {:.3f}".format(float(msg["price"])))
+    def on_close(self):
+        print("-- Goodbye! --")
+
+
 
 auth_client = login_authenticated(sandbox_api_key, sandbox_secret, sandbox_passphrase)
-x = auth_client.get_accounts()
-print(x)
+account = auth_client.get_accounts()
+print(account)
+
+wsClient = myWebsocketClient()
+wsClient.start()
+print(wsClient.url, wsClient.products)
+#while (wsClient.message_count < 500):
+#    print ("\nmessage_count =", "{} \n".format(wsClient.message_count))
+#    time.sleep(1)
+wsClient.close()
