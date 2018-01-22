ac4917160f97133739559d9a4be8c301f7be714e
Ben Lefley <benjaminlefley@s-30-239.flex.volo.net>
2018-01-21 21:20:33 -0600

    made date format in iso

--- gdax_trader.py
+++ gdax_trader.py
@@ -12,10 +12,14 @@ ETH = "ETH-USD"
 COIN = ETH
 HODL = "HODL"
 
-sandbox_api_key = "41bafe8b3899d0f3d6c3ae5b28952636"
-sandbox_secret = "J9Ga2gM03QGTpREb+uXzIVNVxDgEJVo970tee5gq78mUAOl8JiC2TMO0qAV5na25KIuMcEymR43OM8kCxn3JqA=="
-sandbox_passphrase = "lefley"
+#sandbox_api_key = "41bafe8b3899d0f3d6c3ae5b28952636"
+#sandbox_secret = "J9Ga2gM03QGTpREb+uXzIVNVxDgEJVo970tee5gq78mUAOl8JiC2TMO0qAV5na25KIuMcEymR43OM8kCxn3JqA=="
+#sandbox_passphrase = "lefley"
+api_key = ""
+api_secret = ""
+api_passphrase = ""
 
+# WebsocketClient and first function are only needed for seeing real time data, not needed for trader
 class realTimeWebsocketClient(gdax.WebsocketClient):
     def on_open(self):
         self.url = "wss://ws-feed.gdax.com/"
@@ -64,15 +68,14 @@ def getDeltas(auth_client):
     deltas = []
     five_min_prices = []
 
-    end_time = str(datetime.now()) # Get the current time in ISO 8601
-    start_time = str(datetime.now() - timedelta(days = 1)) # Subtract 24 hours from end_time
+    end_time = str(datetime.now().isoformat()) # Get the current time in ISO 8601
+    start_time = str((datetime.now() - timedelta(days = 1)).isoformat()) # Subtract 24 hours from end_time
     time_slice = 300 # represents 5 minutes (see gdax docs)
     five_min_prices = auth_client.get_product_historic_rates(COIN, start = start_time, end = end_time, granularity = time_slice)
 
     for i in range(len(five_min_prices)):
-        if (i == 0)
-            continue # skip first price (no previous price to compare from)
-        deltas[i-1] = five_min_prices[i] - five_min_prices[i-1] # create deltas
+        if (i > 0): # skip first price (no previous price to compare from)
+            deltas[i-1] = five_min_prices[i] - five_min_prices[i-1] # create deltas
 
     return deltas
 
@@ -87,7 +90,7 @@ def testNeuralNetwork(deltas):
     return predicted_delta
 
 def main():
-    auth_client = login(sandbox_api_key, sandbox_secret, sandbox_passphrase)
+    auth_client = login(api_key, api_secret, api_passphrase)
 
     while True:
 
